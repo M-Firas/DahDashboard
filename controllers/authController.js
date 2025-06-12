0cd194b9979aeae372b6f6b1a446a0f40fdd0e0a
Firas M <firas@testing.com>
2025-06-12 15:36:19 +0300

    completed logout functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -141,6 +141,14 @@ const login = async (req, res) => {
 
 // user logout controller
 const logout = async (req, res) => {
+    res.cookie('token', '', {
+        httpOnly: true,
+        expires: new Date(0),
+        secure: true,
+        sameSite: 'none',
+    });
+
+    res.status(StatusCodes.OK).json({ msg: 'Successfully logged out' })
 
 }
 
